ad92d22a653b2c68de1ceb70148232d10d16030a
Harish Narayanan <hnarayanan@gmail.com>
2014-01-09 14:50:01 +0100

    Start making a view for all PDEs, and use it to guide JSON data store

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('pdezengardenApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    .controller('MainCtrl', function ($scope) {
+	$scope.pdes = [
+	    {'name': 'Laplace Equation in 2D',
+	     'thumbnail': 'http://placehold.it/350x250/428bca/fff',
+	     'snippet': 'Harmonic functions from analytic functions.'},
+	    {'name': 'Heat Equation',
+	     'thumbnail': 'http://placehold.it/350x250/428bca/fff',
+	     'snippet': 'Diffusion and smoothing.'},
+	    {'name': 'Backward Heat Equation',
+	     'thumbnail': 'http://placehold.it/350x250/428bca/fff',
+	     'snippet': 'Ill-posed problems and regularization.'},
+	    {'name': 'Constant Coefficient Linear Equations',
+	     'thumbnail': 'http://placehold.it/350x250/428bca/fff',
+	     'snippet': 'Fourier analysis and boundedness.'}
     ];
-  });
+});
