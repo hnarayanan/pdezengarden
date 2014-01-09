76846df9d1340c8f65587edf6197fe266c46b749
Harish Narayanan <hnarayanan@gmail.com>
2014-01-09 19:48:01 +0100

    Allow for MathJax in the PDE view

--- app/scripts/controllers/pde.js
+++ app/scripts/controllers/pde.js
@@ -2,6 +2,7 @@
 
 angular.module('pdezengardenApp')
   .controller('PdeCtrl', function($scope, $routeParams, $http) {
+      MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
       $http.get('data/pdes/' + $routeParams.pdeId + '.json').success(function(data) {
           $scope.pde = data;
         });
