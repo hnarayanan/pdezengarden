e51ba58b881966ecfceff2e230a83ae983b1f9f5
Harish Narayanan <hnarayanan@gmail.com>
2014-01-09 16:07:19 +0100

    Move PDE data to a separate JSON file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,19 +1,9 @@
 'use strict';
 
 angular.module('pdezengardenApp')
-    .controller('MainCtrl', function ($scope) {
-	$scope.pdes = [
-	    {'name': 'Laplace Equation in 2D',
-	     'thumbnail': 'http://placehold.it/350x250/428bca/fff',
-	     'snippet': 'Harmonic functions from analytic functions.'},
-	    {'name': 'Heat Equation',
-	     'thumbnail': 'http://placehold.it/350x250/428bca/fff',
-	     'snippet': 'Diffusion and smoothing.'},
-	    {'name': 'Backward Heat Equation',
-	     'thumbnail': 'http://placehold.it/350x250/428bca/fff',
-	     'snippet': 'Ill-posed problems and regularization.'},
-	    {'name': 'Constant Coefficient Linear Equations',
-	     'thumbnail': 'http://placehold.it/350x250/428bca/fff',
-	     'snippet': 'Fourier analysis and boundedness.'}
-    ];
+    .controller('MainCtrl', function ($scope, $http) {
+	$scope.status = $http;
+	$http.get('data/pdes/pdes.json').success(function(data) {
+	    $scope.pdes = data;
+    });
 });
